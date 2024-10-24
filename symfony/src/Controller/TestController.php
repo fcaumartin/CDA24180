46412012e6aa540fd1e0ff47a4761b263badd57f
frc <fcaumartin@gmail.com>
2024-10-24 11:21:20 +0200

    formulaire

--- symfony/src/Controller/TestController.php
+++ symfony/src/Controller/TestController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Entity\Produit;
+use App\Form\ContactType;
+use App\Form\ProduitType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -27,6 +33,62 @@ class TestController extends AbstractController
         
         return $this->render('test/test2.html.twig', [
             'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/form', name: 'app_form')]
+    public function form(Request $request, EntityManagerInterface $manager): Response
+    {
+        
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $sujet = $data["sujet"];
+            $message = $data["message"];
+            $test = $data["test"];
+
+            $cat = new Categorie();
+            $cat->setNom($sujet);
+            $cat->setImage($message);
+
+            $manager->persist($cat);
+            $manager->flush();
+
+            return $this->redirect("/");
+        }
+
+
+        
+        return $this->render('test/form.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/add_produit', name: 'app_add_produit')]
+    public function add_produit(Request $request, EntityManagerInterface $manager): Response
+    {
+        $p = new Produit();
+        
+        $form = $this->createForm(ProduitType::class, $p);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            // dd($p);
+            $manager->persist($p);
+            $manager->flush();
+
+        }
+
+
+        
+        return $this->render('test/produit.html.twig', [
+            'form' => $form,
         ]);
     }
 }
