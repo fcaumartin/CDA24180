51959a26b24850dadc0f589b30227c07cf3ba27f
frc <fcaumartin@gmail.com>
2024-11-06 11:21:19 +0100

    change function

--- symfony/react/react1/App.jsx
+++ symfony/react/react1/App.jsx
@@ -3,19 +3,21 @@ import { useState } from "react";
 const App = () => {
 
   const [nom, setNom] = useState("au début")
+  const [prenom, setPrenom] = useState("au début")
 
-  function handleChange(event) {
+  const handleChangeNom = (event) => {
     setNom(event.target.value)
   }
 
   return (
     <div>
       <h1 className="rouge">Titre2</h1>
-      Coucou test
+      App1
       <hr />
-      {nom}
+      Bonjour {prenom} {nom}
       <hr />
-      <input type="text" value={nom} onChange={handleChange}/>
+      <input type="text" value={nom} onChange={handleChangeNom}/>
+      <input type="text" value={prenom} onChange={(event) => { setPrenom(event.target.value) }}/>
     </div>
   );
 }
