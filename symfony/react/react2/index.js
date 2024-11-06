2465f6c4f21406421cd7d48d931e9147e482052c
frc <fcaumartin@gmail.com>
2024-11-06 11:50:57 +0100

    react app2

--- symfony/react/react2/index.js
+++ symfony/react/react2/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('react1'));
+const root = ReactDOM.createRoot(document.getElementById('react2'));
 
 root.render(
     <App />
